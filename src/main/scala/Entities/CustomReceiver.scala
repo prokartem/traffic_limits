b27b25ebd979add75cffe59a93b17cf0f1a7a463
ub <artem.prokazow@gmail.com>
2021-07-16 07:58:42 +0300

    add packet header to log level "info"

--- src/main/scala/Entities/CustomReceiver.scala
+++ src/main/scala/Entities/CustomReceiver.scala
@@ -1,5 +1,6 @@
 package Entities
 
+import com.typesafe.scalalogging.Logger
 import org.apache.spark.internal.Logging
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.receiver.Receiver
@@ -7,6 +8,7 @@ import org.pcap4j.core.BpfProgram.BpfCompileMode
 import org.pcap4j.core.PcapNetworkInterface.PromiscuousMode
 import org.pcap4j.core.{PacketListener, PcapNetworkInterface, Pcaps}
 import org.pcap4j.packet.Packet
+import org.slf4j.LoggerFactory
 
 class CustomReceiver(storageLevel: StorageLevel, args: List[String])
     extends Receiver[Long](storageLevel)
@@ -17,6 +19,7 @@ class CustomReceiver(storageLevel: StorageLevel, args: List[String])
 
   override def onStop(): Unit = {}
 
+  private val logger = Logger(LoggerFactory.getLogger(this.getClass))
   private def receive: Unit = {
     val nif: PcapNetworkInterface = Pcaps.findAllDevs().get(0)
     val snapLen: Int              = 65536
@@ -29,8 +32,10 @@ class CustomReceiver(storageLevel: StorageLevel, args: List[String])
 
     while (!isStopped) {
       val listener = new PacketListener {
-        override def gotPacket(packet: Packet): Unit =
+        override def gotPacket(packet: Packet): Unit = {
+          logger.info(packet.getPayload.getHeader.toString)
           store(packet.getRawData.length.toLong)
+        }
       }
       handle.loop(10000000, listener)
     }
